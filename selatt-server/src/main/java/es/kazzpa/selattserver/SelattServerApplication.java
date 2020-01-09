3ca7327f5f36aef45d72412328e2da6aef3d9102
Kazzpa <andres97cb@hotmail.com>
2020-01-09 20:46:39 +0100

    creado server spring docker

---
+++ selatt-server/src/main/java/es/kazzpa/selattserver/SelattServerApplication.java
@@ -0,0 +1,13 @@
+package es.kazzpa.selattserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelattServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelattServerApplication.class, args);
+	}
+
+}
---
+++ selatt-server/src/test/java/es/kazzpa/selattserver/SelattServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.kazzpa.selattserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelattServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
